b92f35ad4b5069a55985f0e947b5b9864f625511
The Vinh <techntvz@gmail.com>
2017-12-12 06:50:48 +0700

    add form add appointment

---
+++ src/AddAppoinment.js
@@ -0,0 +1,69 @@
+import React from 'react';
+
+class AddAppointment extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={}
+    }
+    toogleAppointment(){
+        this.props.onToogleAppointment();
+    }
+    
+    render(){
+        let displayAppointment = {
+            display: this.props.bodyAppointment ? 'block' : 'none'
+        };
+        return (
+            <div className="panel panel-primary">
+            <div className="panel-heading apt-addheading" onClick={ this.toogleAppointment.bind(this) }>
+            <span className="glyphicon glyphicon-plus"></span> Add Appointment</div>
+            <div className="panel-body" style={displayAppointment}>
+              <form className="add-appointment form-horizontal">
+                <div className="form-group">
+                  <label className="col-sm-2 control-label" htmlFor="petName">Pet Name</label>
+                  <div className="col-sm-10">
+                    <input type="text" className="form-control"
+                      id="petName" ref="inputPetName" placeholder="Pet's Name" />
+                  </div>
+                </div>
+                <div className="form-group">
+                  <label className="col-sm-2 control-label" htmlFor="petOwner">Pet Owner</label>
+                  <div className="col-sm-10">
+                    <input type="text" className="form-control"
+                      id="petOwner" ref="inputOwnerName" placeholder="Owner's Name" />
+                  </div>
+                </div>
+                <div className="form-group">
+                  <label className="col-sm-2 control-label" htmlFor="aptDate">Date</label>
+                  <div className="col-sm-4">
+                    <input type="date" className="form-control"
+                      id="aptDate" ref="inputAptDate" />
+                  </div>
+                  <label className="col-sm-2 control-label" htmlFor="aptTime">Time</label>
+                  <div className="col-sm-4">
+                    <input type="time" className="form-control"
+                      id="aptTime" ref="inputAptTime" />
+                  </div>
+    
+                </div>
+                <div className="form-group">
+                  <label className="col-sm-2 control-label" htmlFor="aptNotes">Apt. Notes</label>
+                  <div className="col-sm-10">
+                    <textarea className="form-control" rows="4" cols="50"
+                      id="aptNotes" ref="inputAptNotes" placeholder="Appointment Notes"></textarea>
+                  </div>
+                </div>
+                <div className="form-group">
+                  <div className="col-sm-offset-2 col-sm-10">
+                    <button type="submit" className="btn btn-primary pull-right">Add Appointment</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+
+        )
+    }
+}
+
+export default AddAppointment;
--- src/App.js
+++ src/App.js
@@ -4,27 +4,45 @@ import './App.css';
 import $ from 'jquery';
 import data from './js/data.json';
 import AptList from './AptList';
+import AddAppointment from './AddAppoinment';
 window.jQuery = window.$ = $;
 
 class MainInterface extends Component {
   constructor(props){
     super(props)
     this.state = {
+      bodyAppointmentVisible: false,
       data: data 
     }
   }
 
   deleteItem(id){
+    let deleteUpdate = this.state.data.filter((item,index) => index !== id);
+    this.setState({
+      data: deleteUpdate
+    })
     console.log('click delete ' + id);
   }
-  
+  toogleApointment(){
+    // let tempVisible = !this.state.bodyAppointmentVisible;
+    // this.setState({
+    //   bodyAppointment: tempVisible
+    // })
+    console.log('toogle')
+  }
   render() {    
     return (
       <div className="interface">
         <div className="item-list media-list">
+        <AddAppointment bodyAppointment={this.state.bodyAppointmentVisible}/>
           <ul className="item-list media-list">   
           {
-            this.state.data.map((item, index) =>  <AptList data={item} key={index} index={index} onDelete={this.deleteItem.bind(this)}/>)
+            this.state.data.map((item, index) =>  <AptList data={item} 
+                                                           key={index} 
+                                                           index={index} 
+                                                           onDelete={this.deleteItem.bind(this)}
+                                                           onToogleAppointment={this.toogleApointment.bind(this)}
+                                                  />)
           }        
           </ul>
         </div>
